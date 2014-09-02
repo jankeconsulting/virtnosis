aa2650d2312ff33dd51f968f23169aa2cae8f902
Ralph Janke <virtnosis@jankeconsulting.ca>
2014-09-02 00:45:57 -0400

    Fixed unbalanced save/restore

--- src/domainviewmodel.cpp
+++ src/domainviewmodel.cpp
@@ -5,8 +5,26 @@
  */
 
 #include "domainviewmodel.h"
+#include <QDebug>
 
 DomainViewModel::DomainViewModel(QObject *parent) :
     QStandardItemModel(parent)
 {
+}
+
+QVariant DomainViewModel::data(const QModelIndex &index, int role) const
+{
+    if(role == DomainItem::domainStateRole) {
+        DomainItem *item = static_cast<DomainItem *>(this->item(index.row(), index.column()));
+
+        qDebug() << "DomainViewModel::data: index = " << index;
+        qDebug() << "DomainViewModel::data: item = " << item;
+        if(item)
+            qDebug() << "DomainViewModel::data: hypervisor = " << item->hypervisor();
+
+        return QVariant();
+    } else {
+        return QStandardItemModel::data(index, role);
+    }
+
 }
