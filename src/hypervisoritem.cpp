d47c18f4c7f1eeadce24473818a0d512149e0dad
Ralph Janke <virtnosis@jankeconsulting.ca>
2014-09-18 12:46:16 -0400

    Remove obsolete code

--- src/hypervisoritem.cpp
+++ src/hypervisoritem.cpp
@@ -14,24 +14,6 @@ HypervisorItem::HypervisorItem(Hypervisor *hypervisor) :
 
 void HypervisorItem::addDomainsFromHypervisor(Hypervisor *hypervisor)
 {
-    /*
-    virDomainPtr *domains;
-    int i;
-
-    virConnectPtr connection = hypervisor->connection();
-
-//    qDebug() << "VirtnosisWindow::list_domains: connection = " << connection;
-
-    int number_of_domains = virConnectListAllDomains(connection, &domains, 0);
-//    qDebug() << "VirtnosisWindow::list_domains: number_of_domains = " << number_of_domains;
-
-    if (number_of_domains < 0)
-        qDebug() << "VirtnosisWindow::list_domains: error = " << number_of_domains;
-
-    for (i = 0; i < number_of_domains; i++) {
-*/
-//        qDebug() << "VirtnosisWindow::list_domains: domains[" << i << "] = " << domains[i] << " name = " << virDomainGetName(domains[i]) << " state = " << virDomainIsActive(domains[i]);
-
     DomainItem *domain_item;
 
     QList<Domain *> list = hypervisor->domains();
@@ -48,13 +30,11 @@ void HypervisorItem::addDomainsFromHypervisor(Hypervisor *hypervisor)
         hypervisor_varobj.setValue(*hypervisor);
         QAbstractItemModel *model = this->model();
 
-//        qDebug() << "VirtnosisWindow::list_domains: domain = " << domain;
         QVariant domain_varobj;
         domain_varobj.setValue(*domain);
         model->setData(index, domain_varobj, DomainViewModel::domainDomainRole);
         model->setData(index, hypervisor_varobj, DomainViewModel::domainHypervisorRole);
         model->setData(index, DomainViewModel::typeDomain, DomainViewModel::domainTypeRole);
-//        qDebug() << "VirtnosisWindow::list_domains:  = " << number_of_domains;
     }
 }
 
